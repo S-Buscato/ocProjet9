556f16b90b24447654c00a157ecac95ceabb2e39
S-Buscato <sylvain.buscato@gmail.com>
2021-12-19 22:24:35 +0100

    first commit : mise en place projet

---
+++ src/main/resources/scriptSQL.sql
@@ -0,0 +1,12 @@
+
+CREATE SEQUENCE public.patient_id_seq;
+
+CREATE TABLE public.patient (
+id INTEGER NOT NULL DEFAULT nextval('public.patient_id_seq'),
+dob TIMESTAMP NOT NULL,
+address VARCHAR(100) NOT NULL,
+phone VARCHAR(100) NOT NULL,
+sex CHAR NOT NULL
+);
+
+CREATE SEQUENCE public.patient_id_seq;
