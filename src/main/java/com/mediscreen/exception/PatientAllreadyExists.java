f5c1ccf7ea908b5629a3bdc0e4a187430e0b4511
S-Buscato <sylvain.buscato@gmail.com>
2021-12-26 10:04:16 +0100

    reorg folder

--- src/main/java/com/mediscreen/exception/PatientAllreadyExists.java
+++
@@ -1,9 +0,0 @@
-package com.mediscreen.exception;
-
-import com.mediscreen.constant.Messages;
-
-public class PatientAllreadyExists extends Exception {
-    public PatientAllreadyExists(){
-        super(Messages.USER_ALREADY_IN_YOUR_FRIENDS_LIST);
-    }
-}
