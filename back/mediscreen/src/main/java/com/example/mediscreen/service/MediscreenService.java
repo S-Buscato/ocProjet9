f458886e3f6e7cb5bfa7261051ecd954c746a336
S-Buscato <sylvain.buscato@gmail.com>
2022-01-11 07:24:25 +0100

    debut sprint 3

---
+++ back/mediscreen/src/main/java/com/example/mediscreen/service/MediscreenService.java
@@ -0,0 +1,9 @@
+package com.example.mediscreen.service;
+
+import com.noteapp.model.Note;
+import com.patientapp.model.Patient;
+
+public class MediscreenService {
+    Patient patient = new Patient();
+    Note note = new Note();
+}
